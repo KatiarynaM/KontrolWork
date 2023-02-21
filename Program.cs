498983c467381e39b4086d337de1a264ece6abe4
Katya.M <1321ae7@gmail.com>
2023-02-21 22:36:15 +0300

    Прохождение по массиву

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿string[] Array = {"Katya", "Tim", "Margo", "Yura", "Viktor", "Max"};
+﻿string[] array = {"Katya", "Tim", "Margo", "Yura", "Viktor", "Max"};
+Console.WriteLine($" [{String.Join(",", array)}]");
+int count = array.Length;
+string[] newArray= {""};
+int i =-1;
+for( int a = 0; a <= count-1; a++){
+      string name = array[a];
+      if (name.Length ==3){
+            i++;
+            newArray[i] = name;
+      }
+
+
+}
+Console.WriteLine($" [{String.Join(",", newArray)}]");
+
+
+
+
+/*string[] group = Array.FindAll(Array, Array => Array.Length == 3);
+foreach (var Array in group) Console.WriteLine(Array);*/
