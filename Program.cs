23c86ccc654b1a21c1f2b14d4dc760eb1a6ace75
Katya.M <1321ae7@gmail.com>
2023-02-22 20:28:58 +0300

    Создали ReadMe

--- Program.cs
+++ Program.cs
@@ -1,21 +1,10 @@
-﻿string[] array = {"Katya", "Tim", "Margo", "Yura", "Viktor", "Max"};
+﻿string[] array = {"Katya", "Tim", "Margo", "Yura", "Viktor", "Max", "000",  "))1"};
 Console.WriteLine($" [{String.Join(",", array)}]");
 int count = array.Length;
-string[] newArray= {""};
-int i =-1;
 for( int a = 0; a <= count-1; a++){
       string name = array[a];
       if (name.Length ==3){
-            i++;
-            newArray[i] = name;
+            Console.Write($"{name},  ");
       }
-
-
 }
-Console.WriteLine($" [{String.Join(",", newArray)}]");
-
-
-
-
-/*string[] group = Array.FindAll(Array, Array => Array.Length == 3);
-foreach (var Array in group) Console.WriteLine(Array);*/
+      
